6fcfad5877739d973b6dce576aed86fbd93e802d
Berker Peksag <berker.peksag@gmail.com>
2012-07-10 22:03:56 +0300

    Add a small Python script for convert subtitles to UTF-8.

---
+++ convertutf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import re
+import sys
+
+
+extensions = ('srt', 'sub')
+
+SOURCE_ENCODING = 'iso-8859-9'
+TARGET_ENCODING = 'utf-8'
+
+_slugify = lambda str: re.sub(r'\W+', '-', str.lower())
+
+
+def _convert_utf8(source):
+    source_name, source_ext = os.path.splitext(source)
+    target_name = _slugify(source_name) + '_utf8' + source_ext
+    source_file = open(source)
+    target_file = open(target_name, 'w')
+    target_file.write(unicode(source_file.read(),
+                              SOURCE_ENCODING).encode(TARGET_ENCODING))
+
+
+def main(dir_path):
+    files = os.listdir(dir_path)
+    for _file in files:
+        if _file.endswith(extensions):
+            _convert_utf8(_file)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
