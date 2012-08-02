d5210af3f48e15c01aef0d1319b2a26d9d01b2e5
Berker Peksag <berker.peksag@gmail.com>
2012-08-02 04:20:38 +0300

    Add a script to check last activities of Twitter friends.

---
+++ scripts/twitter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datetime import date, datetime, timedelta
+from sys import argv, exit
+from time import strftime, strptime
+
+import requests
+
+BASE_URL = 'http://api.twitter.com/1/'
+LIMIT = 100
+DATE_RANGE = (date.today() - timedelta(days=30)).isoformat()
+
+
+def convert_iso(date_string):
+    return strftime('%Y-%m-%d', strptime(date_string,
+                                         '%a %b %d %H:%M:%S +0000 %Y'))
+
+
+def request(url):
+    r = requests.get(BASE_URL + url)
+    if r.status_code == requests.codes.bad:
+        r.raise_for_status()
+    return r
+
+
+def get_following_ids(screen_name):
+    r = request('friends/ids.json?screen_name={0:s}'.format(screen_name))
+    return r.json['ids']
+
+
+def get_user_tweets(user_id):
+    r = request('users/show.json?user_id={0:d}'.format(user_id))
+    if 'status' in r.json:
+        result = (r.json['screen_name'],
+                  convert_iso(r.json['status']['created_at']))
+    else:
+        result = (r.json['screen_name'], '<private>')
+    return result
+
+
+def main(screen_name):
+    following_ids = get_following_ids(screen_name)
+
+    for user_id in following_ids[:LIMIT]:
+        screen_name, last_tweet_date = get_user_tweets(user_id)
+        print 'Checking {0:s}...'.format(screen_name)
+        if last_tweet_date <= DATE_RANGE:
+            print screen_name, last_tweet_date
+
+
+def usage():
+    exit('Usage: python {0} <TWITTER_SCREEN_NAME>'.format(__file__))
+
+if __name__ == '__main__':
+    if len(argv) is not 2 or not isinstance(argv[1], basestring):
+        usage()
+    main(argv[1])
