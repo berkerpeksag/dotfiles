c331740ff1f946c98faed8d0f7735b5f720b265b
Berker Peksag <berker.peksag@gmail.com>
2012-09-06 01:28:38 +0300

    Support absolute paths.

--- scripts/convertutf8.py
+++ scripts/convertutf8.py
@@ -14,11 +14,11 @@ TARGET_ENCODING = 'utf-8'
 _slugify = lambda _str: re.sub(r'\W+', '-', _str.lower())
 
 
-def _convert_utf8(source):
+def _convert_utf8(source, dir_path):
     source_name, source_ext = os.path.splitext(source)
     target_name = _slugify(source_name) + '_utf8' + source_ext
-    source_file = open(source)
-    target_file = open(target_name, 'w')
+    source_file = open(dir_path + source)
+    target_file = open(dir_path + target_name, 'w')
     target_file.write(unicode(source_file.read(),
                               SOURCE_ENCODING).encode(TARGET_ENCODING))
     os.remove(dir_path + source)
@@ -28,7 +28,7 @@ def main(dir_path):
     files = os.listdir(dir_path)
     for _file in files:
         if _file.endswith(extensions):
-            _convert_utf8(_file)
+            _convert_utf8(_file, dir_path)
 
 
 def usage():
