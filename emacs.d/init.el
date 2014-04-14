2ac4cc2c574aae457d9f4b5acc4eb748208025e0
Berker Peksag <berker.peksag@gmail.com>
2014-04-14 18:31:55 +0300

    Add duplicate-line defun.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -292,4 +292,43 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 
 (global-set-key (kbd "C-w") 'kill-region-or-backward-kill-word)
 
+
+;; taken from http://stackoverflow.com/a/998472
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+
+  ;; put the point in the lowest line and return
+  (next-line arg))
+
+(global-set-key (kbd "C-d") 'duplicate-line)
+
 ;;; init.el ends here
