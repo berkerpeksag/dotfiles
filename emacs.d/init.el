409f39bb9963e8b8a13a0ff42a8e2fe042dfb968
Berker Peksag <berker.peksag@gmail.com>
2014-05-11 14:13:01 +0300

    Add git-blame-line defun.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -331,4 +331,30 @@ Also fix `backward-kill-word' so that it stops at whitespace."
 
 (global-set-key (kbd "C-d") 'duplicate-line)
 
+
+;; taken from http://jayconrod.com/posts/67/emacs-run-gitblame-on-the-current-line
+(defun git-blame-line ()
+  "Runs `git blame` on the current line and
+   adds the commit id to the kill ring"
+  (interactive)
+  (let* ((line-number (save-excursion
+                        (goto-char (point-at-bol))
+                        (+ 1 (count-lines 1 (point)))))
+         (line-arg (format "%d,%d" line-number line-number))
+         (commit-buf (generate-new-buffer "*git-blame-line-commit*")))
+    (call-process "git" nil commit-buf nil
+                  "blame" (buffer-file-name) "-L" line-arg)
+    (let* ((commit-id (with-current-buffer commit-buf
+                        (buffer-substring 1 9)))
+           (log-buf (generate-new-buffer "*git-blame-line-log*")))
+      (kill-new commit-id)
+      (call-process "git" nil log-buf nil
+                    "log" "-1" "--pretty=%h   %an   %s" commit-id)
+      (with-current-buffer log-buf
+        (message "Line %d: %s" line-number (buffer-string)))
+      (kill-buffer log-buf))
+    (kill-buffer commit-buf)))
+
+(global-set-key (kbd "C-b") 'git-blame-line)
+
 ;;; init.el ends here
